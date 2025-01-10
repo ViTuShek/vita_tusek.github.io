52c4f843135fc22537590dcf703c6f136ec314ba
Oskar-cmyk <oskar.kandy@gmail.com>
2025-01-10 13:57:46 +0100

    ja1

--- astro.config.ts
+++ astro.config.ts
@@ -8,8 +8,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
-  base: "/vita_tusek.github.io",
+  site: "https://Oskar-cmyk.github.io", // your lowercase username goes here
+  base: "/vita_tusek.github.io/",
   output: "static",
   integrations: [
     tailwind({
