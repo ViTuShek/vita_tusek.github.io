8c0871f9acb69d0ed85ec8cb6d101071275fe5a8
Oskar-cmyk <oskar.kandy@gmail.com>
2025-01-11 18:29:38 +0100

    pag

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,8 +1,8 @@
 import type { CollectionEntry } from "astro:content";
 import postFilter from "./postFilter";
 
-const getSortedPosts = (posts: CollectionEntry<"blog">[]) => {
-  return posts
+const getSortedPosts = (works: CollectionEntry<"blog">[]) => {
+  return works
     .filter(postFilter)
     .sort(
       (a, b) =>
